ac2c3eac7a4c39329915ea2a3bcdb4081fb7dbd9
paoloevan <paoloev1900@gmail.com>
2022-10-28 16:11:58 +0200

    add bonus 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,10 @@
-/**
-2- cliccando sul testo dell'item, invertire il valore della proprietà
-done del todo corrispondente (se done era uguale a false, impostare true
-e viceversa)
- */
 
-//import { createApp } from 'https://cdn.jsdelivr.net/npm/vue@3.2.41/dist/vue.global.min.js'
-
-const {createApp} = Vue
+const {createApp} = Vue;
 
 createApp({
     data(){
         return{
-            //message:'ciao',
             newTodo: '',
-            //array di oggetti (todo)
             toDo: [
                 {
                     text: 'fare la spesa',
@@ -37,17 +28,15 @@ createApp({
     methods:{
         removeItem(i){
             console.log('click');
-            this.toDo.splice(i, 1)
+            this.toDo.splice(i, 1);
         },
         addTodo(){
-            //devo pushare in toDo un oggetto con text:newTodo
-            //prima lo creo
             this.a = {
                 text: this.newTodo,
                 done: false
             }
-            this.toDo.unshift(this.a)
-            this.newTodo = ''
+            this.toDo.unshift(this.a);
+            this.newTodo = '';
         }
     }
-}).mount('#app')
+}).mount('#app');
